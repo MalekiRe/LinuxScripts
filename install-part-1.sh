879282b611d2f9bfc134472645cbeafc50f6237a
MalekiRe <pocmalek@gmail.com>
2021-06-30 06:18:17 +0000

    Add files via upload

--- install-part-1.sh
+++ install-part-1.sh
@@ -1,15 +1,13 @@
-pacstrap /mnt base base-devel linux linux-firmware vim grub
+pacstrap /mnt base base-devel linux linux-firmware vim grub networkmanager
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash
-pacman -S network-manager
-y
 systemctl enable NetworkManager
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 passwd
 password
 password
-cat "malekbox" etc/hostname 
+echo "malekbox" > etc/hostname 
 exit
 unmount -R /mnt
 reboot
