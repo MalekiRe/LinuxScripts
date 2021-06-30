06ec966cafc21c7777ac01be10cdda9eee5422c9
MalekiRe <pocmalek@gmail.com>
2021-06-30 06:12:42 +0000

    Add files via upload

--- install-part-1.sh
+++ install-part-1.sh
@@ -1,6 +1,8 @@
-pacstrap /mnt base base-devel linux linux-firmware vim grub network-manager
+pacstrap /mnt base base-devel linux linux-firmware vim grub
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash
+pacman -S network-manager
+y
 systemctl enable NetworkManager
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
